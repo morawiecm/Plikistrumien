bab383a6a26229450e2651ec77a545c58663429d
Mariusz Morawiec <morawiec@vp.pl>
2016-11-19 12:54:40 +0100

    otwieranie pliku lub tworzenie i przechwytywanie pliku

--- Plikistrumien/Program.cs
+++ Plikistrumien/Program.cs
@@ -13,7 +13,32 @@ namespace Plikistrumien
     {
         static void Main(string[] args)
         {
-           FileStream sciezka = new FileStream()
+            FileStream sciezka = null;
+            int length = 10;
+            byte ii = 1;
+            try
+            {
+                sciezka = new FileStream(@"c:\test\test.txt", FileMode.OpenOrCreate); //otwarcie lub utworzenie pliku
+                for (int i = 0; i < length; i++)
+                {
+                    ii++;
+                    sciezka.WriteByte(ii);
+                }
+            }
+            catch(FileNotFoundException e) // wyjatek jezeli nie ma pliku
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e) // ogolne bledy njnizszy w hierarchi (najbardziej ogolny)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sciezka != null) sciezka.Close();
+            }
+           
+            
         }
     }
 }
